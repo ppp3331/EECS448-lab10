320ad64e12b11155113b8b172087b0c160c1337a
Becky Lin <beckylin123@ku.edu>
2021-05-02 11:42:07 -0500

    finish

--- DeletePost.php
+++ DeletePost.php
@@ -1,23 +1,21 @@
 <?php
 $mysqli = new mysqli("mysql.eecs.ku.edu", "beckylin", "yee7iTuh", "beckylin");
-$delete = $_POST["post_id"];
+$delete = $_POST["postid"];
 if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
-else
+for ($i = 0; $i < count($delete); $i++)
 {
-	for ($i = 0; $i < count($delete); $i++){
 	$query = "DELETE FROM Posts WHERE post_id='$delete[$i]'";
 	if ($mysqli->query($query))
 	{
-		echo "Post". $delete[$i] . "is deleted<br>";
+		echo "Post ". $delete[$i]." deleted<br>";
 	}
 	else
 	{
 		echo "Error";
 	}
-}
 }
 $mysqli->close();
 ?>
